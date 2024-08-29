21032b1672b26f5ace94577cc4d27d3897e88e42
JacksonBurns <jwburns@mit.edu>
2024-08-28 22:52:36 -0400

    fastprop sobolev optimization

--- models/fastprop_custom/hopt.py
+++ models/fastprop_custom/hopt.py
@@ -24,12 +24,12 @@ def define_by_run_func(trial):
     trial.suggest_categorical("activation_fxn", ("relu", "leakyrelu"))
     trial.suggest_int("interaction_hidden_size", 400, 3_400, 200)
     trial.suggest_int("num_interaction_layers", 0, 6, 1)
-    interaction = trial.suggest_categorical("interaction_operation", ("concatenation", "multiplication", "subtraction", "addition"))
+    interaction = trial.suggest_categorical("interaction_operation", ("concatenation",))  # , "multiplication", "subtraction", "addition"))
     # if either solute OR solvent has hidden layers (but NOT both), can only do concatenation
     if interaction == "concatenation":
-        trial.suggest_int("num_solute_layers", 0, 6, 1)
+        trial.suggest_int("num_solute_layers", 0, 0)  # 6, 1)
         trial.suggest_int("solute_hidden_size", 200, 3_000, 200)
-        trial.suggest_int("num_solvent_layers", 0, 6, 1)
+        trial.suggest_int("num_solvent_layers", 0, 0)  # 6, 1)
         trial.suggest_int("solvent_hidden_size", 200, 3_000, 200)
     else:
         solute_layers = trial.suggest_int("num_solute_layers", 0, 6, 1)
