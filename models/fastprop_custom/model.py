overhaul with fixed data leak

models/fastprop_custom/model.py
@@ -18,39 +18,6 @@ class Concatenation(torch.nn.Module):
         return torch.cat(batch, dim=1)
 
 
-class Multiplication(torch.nn.Module):
-    def forward(self, batch):
-        return batch[0] * batch[1]
-
-
-class Subtraction(torch.nn.Module):
-    def forward(self, batch):
-        return torch.cat((batch[0] - batch[1], batch[2]), dim=1)
-
-
-class Addition(torch.nn.Module):
-    def forward(self, batch):
-        return torch.cat((batch[0] + batch[1], batch[2]), dim=1)
-
-
-# just had a lecture about transformers, this is kinda like something from that, i guess (not really)
-class PairwiseMax(torch.nn.Module):
-    def forward(self, batch):
-        pairswise_intxns = torch.matmul(batch[0].unsqueeze(2), batch[1].unsqueeze(1))
-        maxes, _ = pairswise_intxns.max(dim=2)
-        return torch.cat((maxes, batch[2]), dim=1)
-
-
-class IndependentVariableAppendingSequential(torch.nn.Sequential):
-    def forward(self, input):
-        embedding, independent_variable = input
-        for module in self:
-            if isinstance(module, torch.nn.Linear):
-                embedding = torch.cat((embedding, independent_variable), dim=1)
-            embedding = module(embedding)
-        return embedding
-
-
 # inputs are normally distributed (by our scaling) so clamping at n
 # is like applying a n-sigma cutoff, i.e. anything else is an outlier
 class ClampN(torch.nn.Module):
@@ -68,7 +35,7 @@ class ClampN(torch.nn.Module):
 def _build_mlp(input_size, hidden_size, act_fun, num_layers):
     modules = []
     for i in range(num_layers):
-        modules.append(torch.nn.Linear(1 + (input_size if i == 0 else hidden_size), hidden_size))
+        modules.append(torch.nn.Linear(input_size if i == 0 else hidden_size, hidden_size))
         if (num_layers == 1) or (i < num_layers - 1):  # no activation after last layer, unless perceptron
             if act_fun == "sigmoid":
                 modules.append(torch.nn.Sigmoid())
@@ -91,18 +58,8 @@ def _build_mlp(input_size, hidden_size, act_fun, num_layers):
 class fastpropSolubility(_fastprop):
     def __init__(
         self,
-        num_solute_layers: int = 0,
-        solute_hidden_size: int = 1_000,
-        num_solvent_layers: int = 0,
-        solvent_hidden_size: int = 1_000,
-        num_interaction_layers: int = 0,
-        interaction_hidden_size: int = 1_000,
-        interaction_operation: Literal[
-            "concatenation",
-            "multiplication",
-            "subtraction",
-            "addition",
-        ] = "concatenation",
+        num_layers: 2,
+        hidden_size: 1_800,
         activation_fxn: Literal["relu", "leakyrelu"] = "relu",
         input_activation: Literal["sigmoid", "tanh", "clamp3"] = "sigmoid",
         num_features: int = 1613,
@@ -141,66 +98,23 @@ class fastpropSolubility(_fastprop):
         self.register_buffer("temperature_means", temperature_means)
         self.register_buffer("temperature_vars", temperature_vars)
 
-        # solute - temperature is concatenated to the input features
-        solute_modules = _build_mlp(num_features, solute_hidden_size, activation_fxn, num_solute_layers)
-        solute_hidden_size = solute_hidden_size if num_solute_layers > 0 else num_features
+        fnn_modules = [Concatenation()]
+        _input_size = num_features * 2 + 1
+        fnn_modules += _build_mlp(_input_size, hidden_size=hidden_size, act_fun=activation_fxn, num_layers=num_layers)
+        fnn_modules.append(torch.nn.Linear(hidden_size if num_layers else _input_size, 1))
 
-        # solvent - temperature is concatenated to the input features
-        solvent_modules = _build_mlp(num_features, solvent_hidden_size, activation_fxn, num_solvent_layers)
-        solvent_hidden_size = solvent_hidden_size if num_solvent_layers > 0 else num_features
-
-        # optionally bound input
-        if input_activation == "clamp3":
-            solute_modules.insert(0, ClampN(n=3.0))
-            solvent_modules.insert(0, ClampN(n=3.0))
-        elif input_activation == "sigmoid":
-            solute_modules.insert(0, torch.nn.Sigmoid())
-            solvent_modules.insert(0, torch.nn.Sigmoid())
-        elif input_activation == "tanh":
-            solute_modules.insert(0, torch.nn.Tanh())
-            solvent_modules.insert(0, torch.nn.Tanh())
-
-        # assemble modules (if empty, just passes input through)
-        self.solute_representation_module = IndependentVariableAppendingSequential(*solute_modules)
-        self.solvent_representation_module = IndependentVariableAppendingSequential(*solvent_modules)
-
-        # interaction module
-        interaction_modules = []
-        if interaction_operation == "concatenation":  # size increases if concatenated
-            num_interaction_features = solvent_hidden_size + solute_hidden_size
-            interaction_modules.append(Concatenation())
-        else:
-            if solute_hidden_size != solvent_hidden_size:
-                raise TypeError(
-                    f"Invalid choice of interaction ({interaction_operation}) for mis-matched solute/solvent"
-                    f" embedding sizes {solute_hidden_size}/{solvent_hidden_size}."
-                )
-            num_interaction_features = solvent_hidden_size
-            if interaction_operation == "multiplication":
-                interaction_modules.append(Multiplication())
-            elif interaction_operation == "subtraction":
-                interaction_modules.append(Subtraction())
-            elif interaction_operation == "addition":
-                interaction_modules.append(Addition())
-            else:
-                raise TypeError(f"Unknown interaction operation '{interaction_operation}'!")
-        self.interaction_module = torch.nn.Sequential(*interaction_modules)
-
-        # solution
-        solution_modules = _build_mlp(num_interaction_features, interaction_hidden_size, activation_fxn, num_interaction_layers)
-        # readout
-        solution_modules.append(torch.nn.Linear(1 + (num_interaction_features if num_interaction_layers == 0 else interaction_hidden_size), 1))
-        self.solution_module = IndependentVariableAppendingSequential(*solution_modules)
-
+        self.clamp = ClampN(n=3.0)
+        self.fnn = torch.nn.Sequential(*fnn_modules)
         self.save_hyperparameters()
 
     def forward(self, batch):
         solute_features, solvent_features, temperature = batch
-        solute_representation = self.solute_representation_module((solute_features, temperature))
-        solvent_representation = self.solvent_representation_module((solvent_features, temperature))
-        solution_representation = self.interaction_module((solute_representation, solvent_representation))
-        y_hat = self.solution_module((solution_representation, temperature))
-        return y_hat
+        return self.fnn((
+            self.clamp(solute_features),
+            self.clamp(solvent_features),
+            temperature,
+        )
+        )
 
     def predict_step(self, batch):
         err_msg = ""
@@ -293,19 +207,6 @@ if __name__ == "__main__":
     temperature = torch.rand((4, 1))
     batch = (solute, solvent, temperature)
 
-    model = fastpropSolubility(
-        num_solute_layers=3,
-        solute_hidden_size=1_400,
-        num_solvent_layers=1,
-        solvent_hidden_size=200,
-        num_interaction_layers=2,
-        interaction_hidden_size=2_400,
-        interaction_operation="pairwisemax",
-        activation_fxn="relu6",
-        num_features=100,
-        learning_rate=0.01,
-        target_means=None,
-        target_vars=None,
-    )
+    model = fastpropSolubility()
     print(model)
     print(model(batch))

models/fastprop_custom/train.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-from astartes import train_val_test_split
+from astartes import train_test_split
 from fastprop.data import fastpropDataLoader, standard_scale
 from fastprop.defaults import ALL_2D, _init_loggers, init_logger
 from fastprop.metrics import (
@@ -98,7 +98,7 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
 
     logger.info(f"Run 'tensorboard --logdir {_output_dir}/tensorboard_logs' to track training progress.")
     random_seed = RANDOM_SEED
-    all_test_results, all_validation_results = [], []
+    all_validation_results = []
     for replicate_number in range(NUM_REPLICATES):
         logger.info(f"Training model {replicate_number+1} of {NUM_REPLICATES} ({random_seed=})")
         # keep backups so repeat trials don't rescale already scaled data
@@ -125,42 +125,35 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
 
         # split the data s.t. model only sees a subset of the studies used to aggregate the training data
         if SPLIT_TYPE == "source":
-            studies_train, studies_val, studies_test = train_val_test_split(pd.unique(metadata_df["source"]), random_state=random_seed)
+            studies_train, studies_val = train_test_split(pd.unique(metadata_df["source"]), random_state=random_seed, train_size=0.95, test_size=0.05)
             train_indexes = metadata_df.index[metadata_df["source"].isin(studies_train)].tolist()
             val_indexes = metadata_df.index[metadata_df["source"].isin(studies_val)].tolist()
-            test_indexes = metadata_df.index[metadata_df["source"].isin(studies_test)].tolist()
         elif SPLIT_TYPE == "solute":
-            solutes_train, solutes_val, solutes_test = train_val_test_split(pd.unique(metadata_df["solute_smiles"]), random_state=random_seed)
+            solutes_train, solutes_val = train_test_split(pd.unique(metadata_df["solute_smiles"]), random_state=random_seed)
             train_indexes = metadata_df.index[metadata_df["solute_smiles"].isin(solutes_train)].tolist()
             val_indexes = metadata_df.index[metadata_df["solute_smiles"].isin(solutes_val)].tolist()
-            test_indexes = metadata_df.index[metadata_df["solute_smiles"].isin(solutes_test)].tolist()
         else:
-            train_indexes, val_indexes, test_indexes = train_val_test_split(np.arange(len(metadata_df)), random_state=random_seed)
+            train_indexes, val_indexes = train_test_split(np.arange(len(metadata_df)), random_state=random_seed)
         _total = len(metadata_df)
         logger.info(
-            f"train: {len(train_indexes)} ({len(train_indexes)/_total:.0%}) validation:"
-            f"{len(val_indexes)} ({len(val_indexes)/_total:.0%}) test: {len(test_indexes)} ({len(test_indexes)/_total:.0%})"
+            f"train: {len(train_indexes)} ({len(train_indexes)/_total:.0%}) validation:" f"{len(val_indexes)} ({len(val_indexes)/_total:.0%})"
         )
         # scaling
         solute_features[train_indexes], solute_feature_means, solute_feature_vars = standard_scale(solute_features[train_indexes])
         solute_scaler = partial(standard_scale, means=solute_feature_means, variances=solute_feature_vars)
         solute_features[val_indexes] = solute_scaler(solute_features[val_indexes])
-        solute_features[test_indexes] = solute_scaler(solute_features[test_indexes])
 
         solvent_features[train_indexes], solvent_feature_means, solvent_feature_vars = standard_scale(solvent_features[train_indexes])
         solvent_scaler = partial(standard_scale, means=solvent_feature_means, variances=solute_feature_vars)
         solvent_features[val_indexes] = solvent_scaler(solvent_features[val_indexes])
-        solvent_features[test_indexes] = solvent_scaler(solvent_features[test_indexes])
 
         temperatures[train_indexes], temperature_means, temperature_vars = standard_scale(temperatures[train_indexes])
         temperature_scaler = partial(standard_scale, means=temperature_means, variances=temperature_vars)
         temperatures[val_indexes] = temperature_scaler(temperatures[val_indexes])
-        temperatures[test_indexes] = temperature_scaler(temperatures[test_indexes])
 
         solubilities[train_indexes], solubility_means, solubility_vars = standard_scale(solubilities[train_indexes])
         target_scaler = partial(standard_scale, means=solubility_means, variances=solubility_vars)
         solubilities[val_indexes] = target_scaler(solubilities[val_indexes])
-        solubilities[test_indexes] = target_scaler(solubilities[test_indexes])
 
         # calculate the expected gradients post-scaling
         # start by inserting the rescaled data back into the metatdata dataframe _in the right order_
@@ -212,16 +205,7 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
             ),
             batch_size=4096,
         )
-        test_dataloader = fastpropDataLoader(
-            SolubilityDataset(
-                solute_features[test_indexes],
-                solvent_features[test_indexes],
-                temperatures[test_indexes],
-                solubilities[test_indexes],
-                tgrads[test_indexes],
-            ),
-            batch_size=4096,
-        )
+        test_dataloader = fastpropDataLoader(SolubilityDataset([], [], [], [], []))
 
         # initialize the model and train/test
         model = fastpropSolubility(
@@ -247,7 +231,6 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
             quiet=remove_output,
             inference_mode=False,
         )
-        all_test_results.append(test_results[0])
         all_validation_results.append(validation_results[0])
 
         random_seed += 1
@@ -256,11 +239,9 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
 
     validation_results_df = pd.DataFrame.from_records(all_validation_results)
     logger.info("Displaying validation results:\n%s", validation_results_df.describe().transpose().to_string())
-    test_results_df = pd.DataFrame.from_records(all_test_results)
-    logger.info("Displaying testing results:\n%s", test_results_df.describe().transpose().to_string())
     if remove_output:
         shutil.rmtree(_output_dir)
-    return validation_results_df, test_results_df
+    return validation_results_df
 
 
 if __name__ == "__main__":
@@ -277,45 +258,14 @@ if __name__ == "__main__":
     #     "solute_hidden_size": 0,
     #     "solvent_hidden_size": 0,
     # }
-    # optimized fastprop-physics model
-    # run with: DISABLE_CUSTOM_LOSS=1
-    # hopt_params = {
-    #     "input_activation": "tanh",
-    #     "activation_fxn": "leakyrelu",
-    #     "interaction_hidden_size": 1200,
-    #     "num_interaction_layers": 2,
-    #     "interaction_operation": "multiplication",
-    #     "num_solute_layers": 3,
-    #     "num_solvent_layers": 3,
-    #     "solute_hidden_size": 800,
-    #     "solvent_hidden_size": 800,
-    # }
     # optimized fastprop-sobolev model
     # run with: DISABLE_CUSTOM_LOSS=0
-    # hopt_params = {
-    #     "input_activation": "clamp3",
-    #     "activation_fxn": "leakyrelu",
-    #     "interaction_hidden_size": 1000,
-    #     "num_interaction_layers": 4,
-    #     "interaction_operation": "concatenation",
-    #     "num_solute_layers": 0,
-    #     "num_solvent_layers": 0,
-    #     "solute_hidden_size": 0,
-    #     "solvent_hidden_size": 0,
-    # }
-    # optimized fastprop-sobolev-physics model
-    # run with: DISABLE_CUSTOM_LOSS=0
-    # hopt_params = {
-    #     "input_activation": "clamp3",
-    #     "activation_fxn": "leakyrelu",
-    #     "interaction_hidden_size": 1000,
-    #     "num_interaction_layers": 1,
-    #     "interaction_operation": "multiplication",
-    #     "num_solute_layers": 1,
-    #     "num_solvent_layers": 2,
-    #     "solute_hidden_size": 1000,
-    #     "solvent_hidden_size": 1000,
-    # }
+    hopt_params = {
+        "input_activation": "clamp3",
+        "activation_fxn": "leakyrelu",
+        "hidden_size": 1000,
+        "num_layers": 4,
+    }
     train_ensemble(
         remove_output=False,
         num_features=1613,