overhauld to fix data leak and produce final aleatoric error plots

models/chemprop/train.py
@@ -21,7 +21,7 @@ from chemprop.nn import metrics
 
 NUM_REPLICATES = 4
 RANDOM_SEED = 1701  # the final frontier
-TRAINING_FPATH = Path("krasnov/bigsoldb_chemprop.csv")
+TRAINING_FPATH = Path("krasnov/bigsoldb_chemprop_nonaq.csv")
 
 
 class CustomMSEMetric(metrics.MSEMetric):
@@ -31,10 +31,16 @@ class CustomMSEMetric(metrics.MSEMetric):
 
 class SobolevMulticomponentMPNN(multi.MulticomponentMPNN):
     def training_step(self, batch, batch_idx):
+        return self._sobolev_loss(batch, "train")
+
+    def validation_step(self, batch, batch_idx):
+        return self._sobolev_loss(batch, "val")
+
+    @torch.enable_grad()
+    def _sobolev_loss(self, batch, name):
         bmg, V_d, X_d, targets, *_ = batch
         # track grad for temperature
         X_d.requires_grad_()
-
         Z = self.fingerprint(bmg, V_d, X_d)
         y_hat = self.predictor.train_step(Z)
         y_loss = torch.nn.functional.mse_loss(y_hat, targets[:, 0, None], reduction="mean")
@@ -46,21 +52,28 @@ class SobolevMulticomponentMPNN(multi.MulticomponentMPNN):
         )
         _scale_factor = 1.0
         y_grad_loss = _scale_factor * (y_grad_hat - targets[:, 1]).pow(2).nanmean()  # MSE ignoring nan
-        loss = y_loss  # + y_grad_loss
-        self.log("train/sobolev_loss", loss)
-        self.log("train/logs_loss", y_loss)
-        self.log("train/grad_loss", y_grad_loss)
-
-        self.log("train_loss", loss, prog_bar=True)
-
+        loss = y_loss + y_grad_loss
+        self.log(f"{name}/sobolev_loss", loss, batch_size=len(batch[0]))
+        self.log(f"{name}/logs_loss", y_loss, batch_size=len(batch[0]))
+        self.log(f"{name}/grad_loss", y_grad_loss, batch_size=len(batch[0]))
+        self.log(f"{name}_loss", loss, prog_bar=True, batch_size=len(batch[0]))
         return loss
 
 
 def _f(r):
     if len(r["scaled_logS"]) == 1:
         return [np.nan]
-    # mask out enormous (non-physical) values and nan/inf
-    return [i if (np.isfinite(i) and np.abs(i) < 1.0) else np.nan for i in np.gradient(r["scaled_logS"], r["scaled_temperature"])]
+    sorted_idxs = np.argsort(r["scaled_temperature"])
+    unsort_idxs = np.argsort(sorted_idxs)
+    # mask out enormous (non-physical) values, negative values, and nan/inf
+    grads = [
+        i if (np.isfinite(i) and np.abs(i) < 1.0 and i > 0.0) else np.nan
+        for i in np.gradient(
+            [r["scaled_logS"][i] for i in sorted_idxs],
+            [r["scaled_temperature"][i] for i in sorted_idxs],
+        )
+    ]
+    return [grads[i] for i in unsort_idxs]
 
 
 def train_ensemble(*, training_percent=None, **model_kwargs):
@@ -72,23 +85,23 @@ def train_ensemble(*, training_percent=None, **model_kwargs):
     _data_dir = Path("../../data")
 
     random_seed = RANDOM_SEED
+    # load the training data
+    src_df = pd.read_csv(_data_dir / TRAINING_FPATH, index_col=0)
+    if training_percent is not None:
+        print(f"Down-sampling training data to {training_percent:.2%} size!")
+        downsample_df = src_df.copy()
+        downsample_df["original_index"] = np.arange(len(src_df))
+        downsample_df = downsample_df.groupby(["solute_smiles", "solvent_smiles", "source"]).aggregate(list)
+        downsample_df = downsample_df.sample(frac=training_percent, replace=False, random_state=random_seed)
+        chosen_indexes = downsample_df.explode("original_index")["original_index"].to_numpy().flatten().astype(int)
+        print(f"Actual downsample percentage is {len(chosen_indexes)/len(src_df):.2%}, count: {len(chosen_indexes)}!")
+        src_df = src_df.iloc[chosen_indexes]
+        src_df.reset_index(inplace=True, drop=True)
+
     all_validation_results = []
     for replicate_number in range(NUM_REPLICATES):
         print(f"Training model {replicate_number+1} of {NUM_REPLICATES} ({random_seed=})")
-        # load the training data
-        df = pd.read_csv(_data_dir / TRAINING_FPATH, index_col=0)
-
-        if training_percent is not None:
-            print(f"Down-sampling training data to {training_percent:.2%} size!")
-            downsample_df = df.copy()
-            downsample_df["original_index"] = np.arange(len(df))
-            downsample_df = downsample_df.groupby(["solute_smiles", "solvent_smiles", "source"]).aggregate(list)
-            downsample_df = downsample_df.sample(frac=training_percent, replace=False, random_state=random_seed)
-            chosen_indexes = downsample_df.explode("original_index")["original_index"].to_numpy().flatten().astype(int)
-            print(f"Actual downsample percentage is {len(chosen_indexes)/len(df):.2%}, count: {len(chosen_indexes)}!")
-            df = df.iloc[chosen_indexes]
-            df.reset_index(inplace=True, drop=True)
-
+        df = src_df.copy()
         # split the data s.t. model only sees a subset of the studies used to aggregate the training data
         studies_train, studies_val = train_test_split(pd.unique(df["source"]), random_state=random_seed, train_size=0.90, test_size=0.10)
         train_indexes = df.index[df["source"].isin(studies_train)].tolist()
@@ -153,7 +166,7 @@ def train_ensemble(*, training_percent=None, **model_kwargs):
 
         # build Chemprop
         mcmp = nn.MulticomponentMessagePassing(
-            blocks=[nn.BondMessagePassing(depth=3, d_h=800) for _ in range(len(all_data))],  # dropout=0.2
+            blocks=[nn.BondMessagePassing(depth=3, d_h=800) for _ in range(len(all_data))],
             n_components=len(all_data),
         )
         agg = nn.MeanAggregation()
@@ -162,7 +175,6 @@ def train_ensemble(*, training_percent=None, **model_kwargs):
             input_dim=mcmp.output_dim + 1,  # temperature
             hidden_dim=800,
             n_layers=2,
-            # dropout=0.5,
             criterion=CustomMSEMetric(),
             output_transform=output_transform,
         )
@@ -175,9 +187,6 @@ def train_ensemble(*, training_percent=None, **model_kwargs):
             batch_norm=True,
             metrics=metric_list,
             X_d_transform=X_d_transform,
-            # init_lr=0.00001,
-            # max_lr=0.00001,
-            # final_lr=0.00001,
         )
         print(mcmpnn)
         try:
@@ -192,15 +201,15 @@ def train_ensemble(*, training_percent=None, **model_kwargs):
         )
         callbacks = [
             EarlyStopping(
-                monitor="val/mse",
+                monitor="val_loss",
                 mode="min",
                 verbose=False,
-                patience=10,
+                patience=15,
             ),
             ModelCheckpoint(
-                monitor="val/mse",
+                monitor="val_loss",
                 dirpath=os.path.join(_output_dir, "checkpoints"),
-                filename=f"repetition-{repetition_number}" + "-{epoch:02d}",
+                filename=f"repetition-{repetition_number}" + "-{epoch:02d}-{val_loss:.02f}",
                 save_top_k=1,
                 mode="min",
             ),
@@ -213,6 +222,8 @@ def train_ensemble(*, training_percent=None, **model_kwargs):
             enable_checkpointing=True,
             check_val_every_n_epoch=1,
             callbacks=callbacks,
+            # to enable sobolev loss during validation
+            inference_mode=False,
         )
 
         trainer.fit(mcmpnn, train_loader, val_loader)
@@ -240,8 +251,7 @@ def rename_recent_dir(updated_name):
 
 
 if __name__ == "__main__":
-    # train_ensemble()
-    # exit(0)
+    # train_ensemble(training_percent=1.0)
     for training_count in (20, 50, 100, 200, 500, 1000, 2000, 3500, 5215):
         training_percent = training_count / 5215
         train_ensemble(training_percent=training_percent)

models/fastprop/classes.py
@@ -1,5 +1,5 @@
 """
-model.py - solubility prediction model definition
+classes.py - solubility prediction model definition and associated classes
 
 batches are organized (solute, solvent, temperature)
 """
@@ -9,8 +9,40 @@ from types import SimpleNamespace
 from typing import Literal
 
 import torch
+from torch.utils.data import Dataset as TorchDataset
 from fastprop.data import inverse_standard_scale, standard_scale
 from fastprop.model import fastprop as _fastprop
+
+
+class SolubilityDataset(TorchDataset):
+    def __init__(
+        self,
+        solute_features: torch.Tensor,
+        solvent_features: torch.Tensor,
+        temperature: torch.Tensor,
+        solubility: torch.Tensor,
+        solubility_gradient: torch.Tensor,
+    ):
+        self.solute_features = solute_features
+        self.solvent_features = solvent_features
+        self.temperature = temperature
+        self.solubility = solubility
+        self.solubility_gradient = solubility_gradient
+        self.length = len(solubility)
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, index):
+        return (
+            (
+                self.solute_features[index],
+                self.solvent_features[index],
+                self.temperature[index],
+            ),
+            self.solubility[index],
+            self.solubility_gradient[index],
+        )
 
 
 class Concatenation(torch.nn.Module):
@@ -251,7 +283,11 @@ class GradPropPhys(fastpropSolubility):
         self.save_hyperparameters()
 
     def forward(self, batch):
-        solute_features, solvent_features, temperature, = batch
+        (
+            solute_features,
+            solvent_features,
+            temperature,
+        ) = batch
         solute_representation = self.solute_fnn((solute_features, temperature))
         solvent_representation = self.solvent_fnn((solvent_features, temperature))
         return self.fnn((solute_representation, solvent_representation, temperature))

models/fastprop/test.py
@@ -11,8 +11,7 @@ from pytorch_lightning import Trainer
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from scipy.stats import pearsonr
 
-from data import SolubilityDataset
-from model import fastpropSolubility
+from classes import SolubilityDataset, fastpropSolubility
 
 RANDOM_SEED = 1701  # the final frontier
 
@@ -48,11 +47,10 @@ def test_ensemble(checkpoint_dir: Path):
         all_models.append(model)
     rmses = []
     for holdout_fpath in (
-        Path("boobier/leeds_acetone.csv"),
-        Path("boobier/leeds_benzene.csv"),
-        Path("boobier/leeds_ethanol.csv"),
-        # Path("krasnov/bigsoldb_downsample.csv"),
-        Path("vermeire/solprop_nonaq.csv"),
+        Path("boobier/leeds_acetone_fastprop.csv"),
+        Path("boobier/leeds_benzene_fastprop.csv"),
+        Path("boobier/leeds_ethanol_fastprop.csv"),
+        Path("vermeire/solprop_fastprop_nonaq.csv"),
     ):
         # load the holdout data
         df = pd.read_csv(Path("../../data") / holdout_fpath, index_col=0)
@@ -107,21 +105,13 @@ def test_ensemble(checkpoint_dir: Path):
 
 if __name__ == "__main__":
     # single runs
-    # test_ensemble(Path("output/gradprop/checkpoints"))
-    # test_ensemble(Path("output/fastprop/checkpoints"))
+    # test_ensemble(Path("output/test/checkpoints"))
     # aleatoric error study
-    sobolev_leeds_results = []
-    nonsobolev_leeds_results = []
-    sobolev_solprop_results = []
-    nonsobolev_solprop_results = []
+    fastprop_sobolev_leeds_results = []
+    fastprop_sobolev_solprop_results = []
     for training_count in (20, 50, 100, 200, 500, 1000, 2000, 3500, 5215):
-        leeds_acetone, leeds_benzene, leeds_ethanol, solprop = test_ensemble(Path(f"output/fastprop_sobolev_{training_count}/checkpoints"))
-        sobolev_leeds_results.append([leeds_acetone, leeds_benzene, leeds_ethanol])
-        sobolev_solprop_results.append(solprop)
         leeds_acetone, leeds_benzene, leeds_ethanol, solprop = test_ensemble(Path(f"output/fastprop_{training_count}/checkpoints"))
-        nonsobolev_leeds_results.append([leeds_acetone, leeds_benzene, leeds_ethanol])
-        nonsobolev_solprop_results.append(solprop)
-    print(f"{sobolev_solprop_results=}")
-    print(f"{nonsobolev_solprop_results=}")
-    print(f"{sobolev_leeds_results=}")
-    print(f"{nonsobolev_leeds_results=}")
+        fastprop_sobolev_leeds_results.append([leeds_acetone, leeds_benzene, leeds_ethanol])
+        fastprop_sobolev_solprop_results.append(solprop)
+    print(f"{fastprop_sobolev_solprop_results=}")
+    print(f"{fastprop_sobolev_leeds_results=}")

models/fastprop/train.py
@@ -22,15 +22,14 @@ from fastprop.metrics import (
 from fastprop.model import train_and_test
 from lightning.pytorch import seed_everything
 
-from data import SolubilityDataset
-from model import fastpropSolubility, GradPropPhys
+from classes import SolubilityDataset, fastpropSolubility
 
 logger = init_logger(__name__)
 
 NUM_REPLICATES = 4
 SPLIT_TYPE = "source"  # solute, random
-RANDOM_SEED = 1701  # the final frontier
-TRAINING_FPATH = Path("krasnov/bigsoldb_downsample.csv")
+RANDOM_SEED = 3511  # the final frontier
+TRAINING_FPATH = Path("krasnov/bigsoldb_fastprop_nonaq.csv")
 
 SOLUTE_COLUMNS: list[str] = ["solute_" + d for d in ALL_2D]
 SOLVENT_COLUMNS: list[str] = ["solvent_" + d for d in ALL_2D]
@@ -39,8 +38,17 @@ SOLVENT_COLUMNS: list[str] = ["solvent_" + d for d in ALL_2D]
 def _f(r):
     if len(r["logS"]) == 1:
         return [np.nan]
-    # mask out enormous (non-physical) values and nan/inf
-    return [i if (np.isfinite(i) and np.abs(i) < 1.0) else np.nan for i in np.gradient(r["logS"], r["temperature"])]
+    sorted_idxs = np.argsort(r["temperature"])
+    unsort_idxs = np.argsort(sorted_idxs)
+    # mask out enormous (non-physical) values, negative values, and nan/inf
+    grads = [
+        i if (np.isfinite(i) and np.abs(i) < 1.0 and i > 0.0) else np.nan
+        for i in np.gradient(
+            [r["logS"][i] for i in sorted_idxs],
+            [r["temperature"][i] for i in sorted_idxs],
+        )
+    ]
+    return [grads[i] for i in unsort_idxs]
 
 
 def logS_within_0_7_percentage(truth: torch.Tensor, prediction: torch.Tensor, ignored: None, multitask: bool = False):
@@ -96,8 +104,23 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
     else:
         solute_features_og, solvent_features_og, temperatures_og, solubilities_og, metadata_df_og = data
 
-    logger.info(f"Run 'tensorboard --logdir {_output_dir}/tensorboard_logs' to track training progress.")
     random_seed = RANDOM_SEED
+    if training_percent is not None:
+        logger.warning(f"Down-sampling training data to {training_percent:.2%} size!")
+        downsample_df = metadata_df_og.copy()
+        downsample_df["original_index"] = np.arange(len(metadata_df_og))
+        downsample_df = downsample_df.groupby(["solute_smiles", "solvent_smiles", "source"]).aggregate(list)
+        downsample_df = downsample_df.sample(frac=training_percent, replace=False, random_state=random_seed)
+        chosen_indexes = downsample_df.explode("original_index")["original_index"].to_numpy().flatten().astype(int)
+        logger.warning(f"Actual downsample percentage is {len(chosen_indexes)/len(metadata_df_og):.2%}, count: {len(chosen_indexes)}!")
+        metadata_df_og = metadata_df_og.iloc[chosen_indexes]
+        metadata_df_og.reset_index(inplace=True, drop=True)
+        solubilities_og = solubilities_og[chosen_indexes]
+        temperatures_og = temperatures_og[chosen_indexes]
+        solute_features_og = solute_features_og[chosen_indexes]
+        solvent_features_og = solvent_features_og[chosen_indexes]
+
+    logger.info(f"Run 'tensorboard --logdir {_output_dir}/tensorboard_logs' to track training progress.")
     all_validation_results = []
     for replicate_number in range(NUM_REPLICATES):
         logger.info(f"Training model {replicate_number+1} of {NUM_REPLICATES} ({random_seed=})")
@@ -107,21 +130,6 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
         temperatures = temperatures_og.detach().clone()
         solute_features = solute_features_og.detach().clone()
         solvent_features = solvent_features_og.detach().clone()
-
-        if training_percent is not None:
-            logger.warning(f"Down-sampling training data to {training_percent:.2%} size!")
-            downsample_df = metadata_df.copy()
-            downsample_df["original_index"] = np.arange(len(metadata_df))
-            downsample_df = downsample_df.groupby(["solute_smiles", "solvent_smiles", "source"]).aggregate(list)
-            downsample_df = downsample_df.sample(frac=training_percent, replace=False, random_state=random_seed)
-            chosen_indexes = downsample_df.explode("original_index")["original_index"].to_numpy().flatten().astype(int)
-            logger.warning(f"Actual downsample percentage is {len(chosen_indexes)/len(metadata_df):.2%}, count: {len(chosen_indexes)}!")
-            metadata_df = metadata_df.iloc[chosen_indexes]
-            metadata_df.reset_index(inplace=True, drop=True)
-            solubilities = solubilities[chosen_indexes]
-            temperatures = temperatures[chosen_indexes]
-            solute_features = solute_features[chosen_indexes]
-            solvent_features = solvent_features[chosen_indexes]
 
         # split the data s.t. model only sees a subset of the studies used to aggregate the training data
         if SPLIT_TYPE == "source":
@@ -208,7 +216,7 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
         test_dataloader = fastpropDataLoader(SolubilityDataset([], [], [], [], []))
 
         # initialize the model and train/test
-        model = GradPropPhys(
+        model = fastpropSolubility(
             **model_kwargs,
             target_means=solubility_means,
             target_vars=solubility_vars,
@@ -244,30 +252,67 @@ def train_ensemble(*, data=None, remove_output=False, training_percent=None, **m
     return validation_results_df
 
 
+# open the output directory, rename the most recent subdir with a new name
+def rename_recent_dir(updated_name):
+    parent_dir = "output"
+    subdirs = [os.path.join(parent_dir, d) for d in os.listdir(parent_dir) if os.path.isdir(os.path.join(parent_dir, d))]
+    most_recent_dir = max(subdirs, key=os.path.getmtime)
+    new_name = os.path.join(parent_dir, updated_name)
+    os.rename(most_recent_dir, new_name)
+
+
 if __name__ == "__main__":
     # optimized fastprop model
     # run with: DISABLE_CUSTOM_LOSS=1
-    hopt_params = {
-        "input_activation": "tanh",
-        "activation_fxn": "relu",
-        "hidden_size": 3000,
-        "num_layers": 4,
-        "solvent_hidden_size": 1000,
-        "solvent_layers": 2,
-        "solute_hidden_size": 1200,
-        "solute_layers": 3,
-    }
-    # optimized fastprop-sobolev model
-    # run with: DISABLE_CUSTOM_LOSS=0
+    # hopt_params = {
+    #     "input_activation": "tanh",
+    #     "activation_fxn": "relu",
+    #     "hidden_size": 3000,
+    #     "num_layers": 4,
+    #     "solvent_hidden_size": 1000,
+    #     "solvent_layers": 2,
+    #     "solute_hidden_size": 1200,
+    #     "solute_layers": 3,
+    # }
+    # optimized fastprop_phys model
+    # disable custom loss
     # hopt_params = {
     #     "input_activation": "clamp3",
-    #     "activation_fxn": "leakyrelu",
-    #     "hidden_size": 3000,
+    #     "activation_fxn": "relu",
+    #     "hidden_size": 2000,
     #     "num_layers": 2,
+    #     "solvent_hidden_size": 2400,
+    #     "solvent_layers": 4,
+    #     "solute_hidden_size": 400,
+    #     "solute_layers": 1,
     # }
-    train_ensemble(
-        remove_output=False,
-        num_features=1613,
-        learning_rate=0.0001,
-        **hopt_params,
-    )
+    # optimized gradpropphys model
+    # enable custom loss
+    # hopt_params = {
+    #     "input_activation": "clamp3",
+    #     "activation_fxn": "relu",
+    #     "hidden_size": 2000,
+    #     "num_layers": 2,
+    #     "solvent_hidden_size": 2400,
+    #     "solvent_layers": 4,
+    #     "solute_hidden_size": 400,
+    #     "solute_layers": 1,
+    # }
+    # optimized fastprop-sobolev model
+    # run with: DISABLE_CUSTOM_LOSS=0
+    hopt_params = {
+        "input_activation": "clamp3",
+        "activation_fxn": "leakyrelu",
+        "hidden_size": 3000,
+        "num_layers": 2,
+    }
+    for training_count in (20, 50, 100, 200, 500, 1000, 2000, 3500, 5215):
+        training_percent = training_count / 5215
+        train_ensemble(
+            training_percent=training_percent,
+            remove_output=False,
+            num_features=1613,
+            learning_rate=0.0001,
+            **hopt_params,
+        )
+        rename_recent_dir(f"fastprop_{training_count}")